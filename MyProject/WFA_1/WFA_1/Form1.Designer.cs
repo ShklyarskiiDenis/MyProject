b32edfdad4c16055bba790b6b27fb93355f2fb03
ShklyarskiiDenis <30911695+ShklyarskiiDenis@users.noreply.github.com>
2017-08-13 21:17:05 +0300

    Initial commit

---
+++ MyProject/WFA_1/WFA_1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WFA_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.methodsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.floodFillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simpleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 365);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.methodsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(476, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // methodsToolStripMenuItem
+            // 
+            this.methodsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.floodFillToolStripMenuItem,
+            this.simpleToolStripMenuItem});
+            this.methodsToolStripMenuItem.Name = "methodsToolStripMenuItem";
+            this.methodsToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.methodsToolStripMenuItem.Text = "Метод закраски";
+            // 
+            // floodFillToolStripMenuItem
+            // 
+            this.floodFillToolStripMenuItem.Name = "floodFillToolStripMenuItem";
+            this.floodFillToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.floodFillToolStripMenuItem.Text = "Затравка";
+            this.floodFillToolStripMenuItem.Click += new System.EventHandler(this.floodFillToolStripMenuItem_Click);
+            // 
+            // simpleToolStripMenuItem
+            // 
+            this.simpleToolStripMenuItem.Name = "simpleToolStripMenuItem";
+            this.simpleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.simpleToolStripMenuItem.Text = "Простой";
+            this.simpleToolStripMenuItem.Click += new System.EventHandler(this.simpleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 389);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem methodsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem floodFillToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simpleToolStripMenuItem;
+    }
+}
+
---
+++ MyProject/WFA_1/WFA_1/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_1
+{
+    /// <summary>
+    /// Основной класс формы
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Контракт на рисование
+        /// </summary>
+        Graphics g;
+
+        /// <summary>
+        /// Множество точек - вершин
+        /// </summary>
+        List<Point> p = new List<Point>();
+
+        /// <summary>
+        /// Затравочная точка
+        /// </summary>
+        Point FloodPoint;
+
+        /// <summary>
+        ///  Конструктор формы
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Сравнение цветов для двух точек
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static bool ColorMatch(Color a, Color b)
+        {
+            return (a.ToArgb() & 0xffffff) == (b.ToArgb() & 0xffffff);
+        }
+
+        /// <summary>
+        /// Закраска многоугольника "затравкой"
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <param name="pt"></param>
+        /// <param name="targetColor"></param>
+        /// <param name="replacementColor"></param>
+        static void FloodFill(Bitmap bmp, Point pt, Color targetColor, Color replacementColor)
+        {
+            // Стэк точек матрицы изображения
+            Queue<Point> q = new Queue<Point>();
+
+            // Помещаем затравочную точку в стэк
+            
+            q.Enqueue(pt);
+            // Пока стэк не пуст
+            while (q.Count > 0)
+            {
+                // Вытаскиваем точку из сиэка
+                Point n = q.Dequeue();
+
+                // Сравниваем с цветом закраски
+                if (!ColorMatch(bmp.GetPixel(n.X, n.Y), targetColor))
+                    continue;
+
+                // Берем соседнюю точку
+                Point w = n, e = new Point(n.X + 1, n.Y);
+
+                // Проверяем на цвета границ, фона и заполнения
+                while ((w.X >= 0) && ColorMatch(bmp.GetPixel(w.X, w.Y), targetColor))
+                {
+                    bmp.SetPixel(w.X, w.Y, replacementColor);
+                    if ((w.Y > 0) && ColorMatch(bmp.GetPixel(w.X, w.Y - 1), targetColor))
+                        q.Enqueue(new Point(w.X, w.Y - 1));
+                    if ((w.Y < bmp.Height - 1) && ColorMatch(bmp.GetPixel(w.X, w.Y + 1), targetColor))
+                        q.Enqueue(new Point(w.X, w.Y + 1));
+                    w.X--;
+                }
+
+                // Проверяем на цвета границ, фона и заполнения
+                while ((e.X <= bmp.Width - 1) && ColorMatch(bmp.GetPixel(e.X, e.Y), targetColor))
+                {
+                    bmp.SetPixel(e.X, e.Y, replacementColor);
+                    if ((e.Y > 0) && ColorMatch(bmp.GetPixel(e.X, e.Y - 1), targetColor))
+                        q.Enqueue(new Point(e.X, e.Y - 1));
+                    if ((e.Y < bmp.Height - 1) && ColorMatch(bmp.GetPixel(e.X, e.Y + 1), targetColor))
+                        q.Enqueue(new Point(e.X, e.Y + 1));
+                    e.X++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вызов закраски
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private void FloodFillColorize(int x, int y)
+        {
+            // Получение изображения для обработки
+            Bitmap b = new Bitmap(pictureBox1.Image);
+            // Затравка
+            FloodFill(b, new Point(x, y), Color.White, Color.Blue);
+            // Выгрузка результата на форму
+            pictureBox1.Image = b;
+            pictureBox1.Invalidate();
+            MessageBox.Show("Готово!");
+        }
+
+        void SimpleColorize()
+        {
+            Bitmap b = new Bitmap(pictureBox1.Image);
+            GraphicsPath gp;
+            gp = new GraphicsPath();
+            gp.AddPolygon(p.ToArray());
+            Region region = new Region(gp);
+            for (int i = 0; i < b.Height; i++)
+            {
+                for (int j = 0; j < b.Width - 1; j++)
+                {
+                    Point pp = new Point(j, i);
+                    if (region.IsVisible(pp))
+                    {
+                        b.SetPixel(j, i, Color.Blue);
+                    }
+                    else
+                    {
+                        b.SetPixel(j, i, Color.White);
+                    }
+                }
+            }
+            pictureBox1.Image = b;
+            pictureBox1.Invalidate();
+            MessageBox.Show("Готово!");
+        }
+
+        /// <summary>
+        /// Перерисовка сцены
+        /// </summary>
+        private void Redraw()
+        {
+            // Очистка холста
+            ClearPic();
+            g = Graphics.FromImage(pictureBox1.Image);
+            // Обход всех точек вершин
+            foreach(var i in p)
+            {
+                g.FillEllipse(
+                    new SolidBrush(Color.Black)
+                    , i.X - 5
+                    , i.Y - 5
+                    , 10
+                    , 10
+                    );
+            }
+            // Первая линия (когда точек всего две)
+            if (p.Count == 2)
+                g.DrawLine(
+                    new Pen(Color.Black, 3)
+                    , new Point(p[0].X, p[0].Y)
+                    , new Point(p[1].X, p[1].Y)
+                    );
+            // Остальные точки (когда их больше двух)
+            if (p.Count > 2)
+                g.DrawPolygon(
+                    new Pen(Color.Black, 3)
+                    , p.ToArray()
+                    );
+            pictureBox1.Invalidate();            
+        }
+
+        // Полная очистка холста
+        private void ClearPic()
+        {
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+
+            gr.Clear(Color.White);
+            gr.Dispose();
+
+            pictureBox1.Invalidate();
+        }
+
+        /// <summary>
+        /// Обработчик загрузки формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Создание инструментов
+            g = CreateGraphics();
+            // Сглаживание рисунка
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            // Создание изображения
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            // Вывод очищенного на форму
+            pictureBox1.Image = b;
+            pictureBox1.Invalidate();
+            // Обнуление всего
+            ClearPic();
+            FloodPoint.X = 0;
+            FloodPoint.Y = 0;
+
+            // 2
+            floodFillToolStripMenuItem.Enabled = false;
+        }
+
+        /// <summary>
+        /// Не используется пока
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Обработчик события нажатия на кнопку мыши (правая рисует, левая закрашивает)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Левая кропка
+            if (e.Button == MouseButtons.Left)
+            {
+                // Добавляем точку
+                p.Add(new Point(e.X, e.Y));
+                // Перерисовываем сцену
+                Redraw();
+            }
+            // Правая кновка
+            if (e.Button == MouseButtons.Right)
+            {
+                //2
+                floodFillToolStripMenuItem.Enabled = true;
+
+
+                FloodPoint = new Point(e.X, e.Y);
+                string msg = "Затравочная точка поставлена в координатах : "
+                    + "X = "
+                    + FloodPoint.X.ToString()
+                    + ", Y = "
+                    + FloodPoint.Y.ToString();
+                MessageBox.Show(msg);
+                Redraw();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void floodFillToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 1
+            //if ((FloodPoint.X == 0) && (FloodPoint.Y == 0))
+            //{
+            //    MessageBox.Show("Поставьте затравочную точку правой кнопкой мышки!");
+            //}
+            //else
+            //{
+            //    FloodFillColorize(FloodPoint.X, FloodPoint.Y);
+            //}
+            // 2
+            FloodFillColorize(FloodPoint.X, FloodPoint.Y);
+        }
+
+        private void simpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SimpleColorize();
+        }
+    }
+}
